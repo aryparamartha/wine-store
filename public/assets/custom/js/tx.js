753a2f49f2c014971e93ded3b7ed204a83078d5e
unknown <aryparamartha@gmail.com>
2021-09-22 10:14:13 +0800

    fix edit transaction backwards for final price and sub total

--- public/assets/custom/js/tx.js
+++ public/assets/custom/js/tx.js
@@ -41,8 +41,8 @@ function getItemTemplate(){
             Rp<input style="display: inline-block; width: 100px !important" type="text" class="cart-disc-price form-control" />
             </div>
         </td>
-        <td class="cart-sub-total text-right"></td>
         <td class="cart-final-price text-right"></td>
+        <td class="cart-sub-total text-right"></td>
         <td>
             <button type="button" class="btn-dlt-cart btn btn-danger btn-icon" data-title="Delete Product" data-text="Are you sure you want to delete this data?">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
@@ -123,6 +123,8 @@ $(function() {
 
         // let sub_total = price - qty;
         // sub_total = sub_total - disc_price;
+
+        console.log("calc-sub-tot")
 
         let price_adisc = price - disc_price;
         let sub_total = price_adisc * qty;
